6baa3abb70ffbcc348586266b72b87739cae6f8d
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-05-28 11:11:59 +0200

    Adds project link to homepage, install sass bootstrap, adds logo to homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+function HomePage() {
+  return (
+    <section>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/elinkrull/project-exam-2"
+            target="_blank"
+            rel="noopener noreferrer">
+            Prosjekt 1 – Project Exam 2
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/elinkrull/SemesterProject2"
+            target="_blank"
+            rel="noopener noreferrer">
+            Prosjekt 2 – Semester Project 2
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/elinkrull/cr-ca-js-framework"
+            target="_blank"
+            rel="noopener noreferrer">
+            Project 3 - JavaScript Frameworks
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default HomePage;
