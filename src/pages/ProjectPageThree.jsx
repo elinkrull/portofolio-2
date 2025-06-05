071c5fa881846ad22d2422cc252bcdde9a0a37bf
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2025-06-05 22:08:11 +0200

    Adds content to ProjectPageThree

--- src/pages/ProjectPageThree.jsx
+++ src/pages/ProjectPageThree.jsx
@@ -1,19 +1,23 @@
 import Layout from "../components/Layout";
+import ProjectDetails from "../components/ProjectDetails";
 
-function ProjectThree() {
+function ProjectTwo() {
   return (
     <Layout>
-      <h1>Project Exam 2</h1>
-      <p>
-        Each project article page must include the following: Project article
-        title, detailed description and share/copy link or icon. Project article
-        image with caption (png or jpeg and optimized to). Project link to the
-        live site (must open in a new tab). The GitHub repository README.md page
-        link (must open in a new tab). Main content (reflections and
-        improvements).
-      </p>
+      <ProjectDetails
+        title="Project Exam"
+        image="/printscreen3.png"
+        imageAlt="Project Exam"
+        imageCaption="A booking site built with HTML, CSS bootstrap, JS and React/Vite"
+        description="This accommodation booking application was developed as part of my final exam project. The goal was to design, plan, and build a fully functional frontend application for Holidaze using React, Bootstrap, and the provided API. The project includes both a customer-facing and admin-facing interface where users can browse venues, book accommodations, and venue managers can create and manage their listings. The project was designed in Figma, organized with a Kanban board, and deployed to Netlify."
+        improvements
+        and
+        reflections=""
+        liveUrl="/"
+        githubUrl="/"
+      />
     </Layout>
   );
 }
 
-export default ProjectThree;
+export default ProjectTwo;
